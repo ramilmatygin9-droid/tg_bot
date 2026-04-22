3e8d456116708e5753c7bdbf5d1d34f312810582
GLITCH-WIN <ramilmatygin9@gmail.com>
2026-04-23 01:16:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import telebot
+import os
 
+# Берем токен из переменных Railway
+TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, "Бот работает! 🚀 Я запущен на хостинге 24/7.")
+
+@bot.message_handler(func=lambda message: True)
+def echo_all(message):
+    bot.reply_to(message, f"Ты написал: {message.text}")
+
+if __name__ == "__main__":
+    print("Бот запускается...")
+    bot.infinity_polling()
