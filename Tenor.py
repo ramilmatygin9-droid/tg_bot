fea21a452443949ae43293db5c3b394399749975
GLITCH-WIN <ramilmatygin9@gmail.com>
2026-05-06 18:04:43 +0700

    Update Tenor.py

--- Tenor.py
+++ Tenor.py
@@ -6,7 +6,7 @@ from aiogram.filters import Command
 from aiogram.enums import ParseMode
 
 # --- НАСТРОЙКИ ---
-TOKEN = "8156857401:AAHiNO7znRig1ttMSiJ3joAPHhUr--Z213g" # Сбрось его в @BotFather!
+TOKEN = "8156857401:AAGxshuoGT-sV6hgMfpnFmQAHF7BsAjINjk" # Сбрось его в @BotFather!
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
